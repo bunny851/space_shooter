d8ee47562a3ba9cfad68dbc7c632fb5b4e77207e
Shreyan <shreyansarkar.20@nshm.edu.in>
2022-04-24 16:34:47 +0530

    Parallax background fixed...
    Some minor modifications...

--- Space Shooter Game/Assets/_Space shooter game/Scripts/bgscroll.cs
+++ Space Shooter Game/Assets/_Space shooter game/Scripts/bgscroll.cs
@@ -6,15 +6,17 @@ public class bgscroll : MonoBehaviour
 {
     [SerializeField] private float m_speed = 4f;
     [SerializeField] private float m_bounds = -7.69f;
+    [SerializeField] private Transform m_resetPosition;
+
     private Vector3 StartPosition;
     // Start is called before the first frame update
     void Start()
     {
-        StartPosition = transform.position;
+        StartPosition = m_resetPosition.position;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         transform.Translate(Vector3.down * m_speed * Time.deltaTime);
         if(transform.position.y < m_bounds)
